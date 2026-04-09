d46d990315bc30c02944d69ecaac72b2658ce46d
Henry <83106787+henrylin03@users.noreply.github.com>
2026-04-09 14:13:00 +1000

    style: use Prettier/ESLint instead of Biome (#4)
    * feat(Header): add header for Chinese home page.
    
    at the moment, no need for cta yet, so is just the heading and image (which is an anchor because expected pattern) with background colour. this fixes partially #2.
    
    * chore: start adding seo metadata
    
    * chore: uninstall biome
    
    * style: install prettier and apply
    
    also included prettier plugin for astro and tailwindcss
    
    * style: have prettier run every commit
    
    using existing husky/lint-staged set up
    
    * style: configure linting using eslint
    
    issue when installing a11y astro/eslint plugin, but this can just be ignored for now.
    
    * style: configure linting using eslint
    
    issue when installing a11y astro/eslint plugin, but this can just be ignored for now.
    
    * chore: remove other changes in branch
    
    these were pushed from separate feature branch by accident. removed them to keep this branch (and its PR) pure.

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default [
+  js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+
+  {
+    rules: {
+      "astro/no-set-html-directive": "error",
+      "astro/no-unused-css-selector": "warn",
+      "astro/prefer-class-list-directive": "warn",
+    },
+  },
+];
