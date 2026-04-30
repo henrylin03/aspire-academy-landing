d990d2561d59c7924ae7af48fbd77cae4fe1468b
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-04-30 21:26:18 +1000

    feat(classCard): add class cards to section
    this is mapped via props drilled in. good to have separation between layers

--- src/data/classOfferings.ts
+++ src/data/classOfferings.ts
@@ -13,12 +13,12 @@ export const CLASSES_DATA: ClassOffering[] = [
     imgSrc: groupClassImg,
     title: "small classes",
     description:
-      "Learn with students of your own age group, starting from Year 4. Classes are kept small (max 6 students) to ensure meaningful speaking time and feedback during frequent practice debates",
+      "Learn with students of your own age group, starting from Year 4. Classes are kept small (max 6 students) to ensure meaningful speaking time and feedback during frequent practice debates.",
   },
   {
     imgSrc: soloImg,
     title: "1:1 private coaching",
     description:
-      "Get the maximum opportunity for tailoed feedback in weekly, one-on-one private coaching, helping you get into your school teams, win more debates and sharpen your speaking.",
+      "Get the maximum opportunity for tailored feedback in weekly, one-on-one private coaching, helping you get into your school's debating teams, win more debates and sharpen your speaking.",
   },
 ];
