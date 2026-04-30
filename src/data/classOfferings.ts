3677d3e0bb97149c9f93cf06b1fb8fb66c975c38
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-04-30 21:14:02 +1000

    feat: add images for class offerings

--- src/data/classOfferings.ts
+++ src/data/classOfferings.ts
@@ -1,4 +1,6 @@
 import type { ImageMetadata } from "astro";
+import groupClassImg from "@images/group-call.jpg";
+import soloImg from "@images/happy-online-student.jpg";
 
 export type ClassOffering = {
   title: string;
@@ -8,13 +10,13 @@ export type ClassOffering = {
 
 export const CLASSES_DATA: ClassOffering[] = [
   {
-    imgSrc: "",
+    imgSrc: groupClassImg,
     title: "small classes",
     description:
       "Learn with students of your own age group, starting from Year 4. Classes are kept small (max 6 students) to ensure meaningful speaking time and feedback during frequent practice debates",
   },
   {
-    imgSrc: "",
+    imgSrc: soloImg,
     title: "1:1 private coaching",
     description:
       "Get the maximum opportunity for tailoed feedback in weekly, one-on-one private coaching, helping you get into your school teams, win more debates and sharpen your speaking.",
