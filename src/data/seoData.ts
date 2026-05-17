94d8900ad2b1767a2018517ede06cee07377ce55
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-17 16:01:44 +1000

    feat: apply logic that only years 4 and above can enrol

--- src/data/seoData.ts
+++ src/data/seoData.ts
@@ -8,9 +8,10 @@ export type SeoData = {
 
 export const SEO_MAP: Record<string, SeoData> = {
   "/": {
-    title: "Online Debating Classes | Years 3+ | Aspire Academy by Huaxia",
+    title:
+      "Online Debating Classes | NSW Students Years 4+ | Aspire Academy by Huaxia",
     description:
-      "Online debating and public speaking classes for NSW students in Year 3 and above. Specialist preparation for ISDA, GPS, and PDC (Premier's Debating Challenge) competitions. Learn to think critically, rebut tactically and become a stronger and more confident speaker.",
+      "Online debating and public speaking classes for NSW students in Year 4 and above. Specialist preparation for ISDA, GPS, and PDC (Premier's Debating Challenge) competitions. Learn to think critically, rebut tactically and become a stronger and more confident speaker.",
     keywords: [
       "sydney debating",
       "online debating",
@@ -26,9 +27,9 @@ export const SEO_MAP: Record<string, SeoData> = {
   },
   "/enrol": {
     title:
-      "Enrol in Online Debating Classes | Suitable for NSW students Years 3 and above | Aspire Academy by Huaxia",
+      "Enrol in Online Debating Classes | NSW Students Years 4+ | Aspire Academy by Huaxia",
     description:
-      "Enrol in online debating and public speaking classes, suitable for students in NSW in Years 3 and above, looking to compete in competitions like PDC, ISDA, FED, Eastside and GPS. Includes small-sized classes filled with speaking games and core debating concepts, as well as private debating coaching for more tailored support. Suitable for students ranging from zero experience to competitors.",
+      "Enrol in online debating and public speaking classes, suitable for students in NSW in Years 4 and above, looking to compete in competitions like PDC, ISDA, FED, Eastside and GPS. Includes small-sized classes filled with speaking games and core debating concepts, as well as private debating coaching for more tailored support. Suitable for students ranging from zero experience to competitors.",
     keywords: [
       "enrol sydney debating",
       "enrol online debating classes",
